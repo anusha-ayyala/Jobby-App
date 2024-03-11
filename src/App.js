1c51b6629e59ca944f05fa78f4c74f593ce49379
anusha <anusha.ayyala@gmail.com>
2024-03-11 08:53:19 +0000

    jobby

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {Route, Switch, Redirect} from 'react-router-dom'
 import Login from './components/Login'
 import Home from './components/Home'
 import Jobs from './components/Jobs'
+
 import JobItemDetails from './components/JobItemDetails'
 import NotFound from './components/NotFound'
 import ProtectedRoute from './components/ProtectedRoute'
